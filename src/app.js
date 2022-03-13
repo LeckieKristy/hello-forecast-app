089e826f97cc754fee3f4e3e5841ec725aee6978
LeckieKristy <bkris030@gmail.com>
2022-03-13 16:12:57 -0500

    JS update api intergration

--- src/app.js
+++ src/app.js
@@ -23,21 +23,40 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = ` <div class="row">`;
-  let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
-  days.forEach(function (day) {
+  forecast.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
       `  <div class="col-2">
-                  <div class="weather-forecast-day">${day}</div>
-                  <div class="weather-forcast-icon">ICON</div>
+                  <div class="weather-forecast-day">${formatDay(
+                    forecastDay.dt
+                  )}</div>
+                  <img
+                  src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
+                  alt="
+                  width="40/>
+
                   <div class="weather-forcast-temperature">
-                    <span class="weather-forecast-temperature-max">18</span>
+                    <span class="weather-forecast-temperature-max">${Math.round(
+                      forecastDay.temp.max
+                    )}</span>
                     |
-                    <span class="weather-forecast-temperature-min">12</span>
+                    <span class="weather-forecast-temperature-min">${Math.round(
+                      forecastDay.temp.min
+                    )}</span>
                   </div>
               </div>`;
   });
